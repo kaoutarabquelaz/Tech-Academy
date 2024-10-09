b901e26bfdfdc566b036f0081f9e783870b5610e
kaoutarabquelaz <Kaoutar.Abquelaz@my-aolcc.com>
2024-10-09 22:37:15 +0300

    Add files via upload

---
+++ Final Python Challange 2.py
@@ -0,0 +1,10 @@
+# Create a list of string items
+items = ['banana', 'apple', 'orange', 'mango', 'strawberry', 'grape', 'blueberry', 'kiwi', 'cherry', 'pear']
+
+# Sort the list alphabetically
+items.sort()
+
+# Display the alphabetized list
+print("Alphabetized list:")
+for item in items:
+    print(item)
